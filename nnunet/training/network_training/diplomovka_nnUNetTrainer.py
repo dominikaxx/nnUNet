12eacb0901cee74c445396a489bbe58f6b229e14
dominikaxx <domca.moly@gmail.com>
2022-11-13 20:33:29 +0100

    added axial attention

--- nnunet/training/network_training/diplomovka_nnUNetTrainer.py
+++ nnunet/training/network_training/diplomovka_nnUNetTrainer.py
@@ -1,4 +1,5 @@
-﻿from nnunet.network_architecture.generic_UNet import Generic_UNet
+﻿from nnunet.network_architecture.axial_attention_UNet import Axial_attention_UNet
+from nnunet.network_architecture.generic_UNet import Generic_UNet
 from nnunet.network_architecture.initialization import InitWeights_He
 from nnunet.training.loss_functions.dice_loss import DC_and_BCE_loss
 from nnunet.training.network_training.competitions_with_custom_Trainers.BraTS2020.nnUNetTrainerV2BraTSRegions_moreDA import \
@@ -26,9 +27,8 @@ class diplomovka_nnUNetTrainer2(nnUNetTrainerV2BraTSRegions_DA4_BN_BD):
         self.loss = DC_and_BCE_loss({}, {'batch_dice': True, 'do_bg': True, 'smooth': 0})
 
 
-class diplomovka_nnUNetTrainer_DA4_BN_BD_largeUnet(diplomovka_nnUNetTrainer2):
+class diplomovka_largeUnet(diplomovka_nnUNetTrainer2):
     def initialize_network(self):
-        """inference_apply_nonlin to sigmoid + larger unet"""
         if self.threeD:
             conv_op = nn.Conv3d
             dropout_op = nn.Dropout3d
@@ -53,3 +53,37 @@ class diplomovka_nnUNetTrainer_DA4_BN_BD_largeUnet(diplomovka_nnUNetTrainer2):
         if torch.cuda.is_available():
             self.network.cuda()
         self.network.inference_apply_nonlin = nn.Sigmoid()
+
+
+class diplomovka_axialAttention(diplomovka_nnUNetTrainer2):
+    def initialize_network(self):
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.BatchNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.BatchNorm2d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+
+        self.network = Axial_attention_UNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                                            len(self.net_num_pool_op_kernel_sizes),
+                                            self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
+                                            dropout_op_kwargs,
+                                            net_nonlin, net_nonlin_kwargs, True, False, lambda x: x,
+                                            InitWeights_He(1e-2),
+                                            self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True,
+                                            True,
+                                            encoder_scale=1,
+                                            axial_attention=True, heads=1, dim_heads=4, volume_shape=(128, 128, 128),
+                                            no_attention=[0], axial_bn=True, sum_axial_out=True)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        self.network.inference_apply_nonlin = nn.Sigmoid()
+        # self.network.inference_apply_nonlin = softmax_helper
