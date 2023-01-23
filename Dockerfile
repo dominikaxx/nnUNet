fc5c895bb3f3d5ade57435751479b60e5c42bda7
dominikaxx <domca.moly@gmail.com>
2023-01-23 17:57:09 +0100

    dockerfile len predikcia bez konvertovania labels na brats konvenciu

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 # Parent Image
 FROM nvcr.io/nvidia/pytorch:20.11-py3
 
-# Environment Variables:
-#ENV nnUNet_raw_data_base "/mnt/grafika/nnUNet_raw_data_base"
-#ENV nnUNet_preprocessed "/mnt/grafika/nnUNet_preprocessed"
-#ENV RESULTS_FOLDER "/mnt/grafika/nnUNet_trained_models"
-
 # Installing nnU-Net
 RUN git clone -b DP --single-branch https://github.com/dominikaxx/nnUNet.git
 WORKDIR /workspace/nnUNet
@@ -32,56 +27,4 @@ USER grafika
 
 ENTRYPOINT ["python3", "/workspace/nnUNet/predikuj.py"]
 
-
-
-
-
-
-
-
-#FROM nvidia/cuda:11.6.2-base-ubuntu20.04
-#RUN apt-get update && apt install -y software-properties-common
-#RUN add-apt-repository ppa:deadsnakes/ppa
-#RUN apt-get update
-#RUN apt install -y python3.8
-#RUN apt install -y python3-pip
-#RUN apt install -y git
-#COPY requirements.txt /usr/local/bin
-#RUN pip3 install -r /usr/local/bin/requirements.txt
-#RUN pip3 install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 -f https://download.pytorch.org/whl/torch_stable.html
-#COPY nnunet /usr/local/bin/nnUNet/
-#COPY predikuj.py /usr/local/bin/nnUNet/
-#COPY setup.py /usr/local/bin/nnUNet/
-#
-#COPY nnUNet_trained_models /usr/local/bin/trained_models/
-#RUN pip3 install -U setuptools
-#RUN pip3 install -e /usr/local/bin/nnUNet
-#ENV RESULTS_FOLDER=/usr/local/bin/trained_models/
-#COPY predikuj.py /usr/local/bin
-#
-#ENTRYPOINT ["python3", "/usr/local/bin/predikuj.py"]
-
-#FROM python:slim-buster
-#
-#RUN apt-get update && apt-get install -y --no-install-recommends apt-utils ca-certificates wget unzip git
-#RUN update-ca-certificates
-#
-#WORKDIR /nnunet
-#RUN pip3 install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 -f https://download.pytorch.org/whl/torch_stable.html
-#
-#RUN pip3 install nnunet
-#COPY setup.sh /nnunet
-#COPY SegModel.zip /nnunet
-#
-#RUN mkdir -p /nnunet/data/input/nnUNet_raw_data
-#RUN mkdir -p /nnunet/data/input/nnUNet_preprocessed
-#RUN mkdir -p /nnunet/data/output
-#
-#ENV nnUNet_raw_data_base=/nnunet/data/input/
-#ENV nnUNet_preprocessed=/nnunet/data/input/nnUNet_preprocessed
-#ENV RESULTS_FOLDER=/nnunet/data/output/
-#
-#RUN nnUNet_install_pretrained_model_from_zip SegModel.zip
-#
-#COPY predikuj.py /nnunet/
 
--- predikuj.py
+++ predikuj.py
@@ -9,30 +9,30 @@ def main():
     input_folder = '/input'
     output_folder = '/output'
 
-    tmp_input_folder = '/tmp_input'
-    tmp_output_folder = '/tmp_output'
-    maybe_mkdir_p(tmp_input_folder)
+    # tmp_input_folder = '/tmp_input'
+    # tmp_output_folder = '/tmp_output'
+    # maybe_mkdir_p(tmp_input_folder)
     os.system("export RESULTS_FOLDER=/workspace/nnUNet/nnUNet_trained_models/")
     # convert raw data to nnunet format
-    contrast_to_number = {'t1': '0000', 't1ce': '0001', 't2': '0002', 'flair': '0003'}
-    for p in subfiles(input_folder, join=False):
-        tokens = p.split('_')
-        patient_id = tokens[0] + "_" + tokens[1]
-        contrast = tokens[-1].split('.')[0]
-        shutil.copy(join(input_folder, p),
-                    join(tmp_input_folder, patient_id + "_" + contrast_to_number[contrast] + ".nii.gz"))
+    # contrast_to_number = {'t1': '0000', 't1ce': '0001', 't2': '0002', 'flair': '0003'}
+    # for p in subfiles(input_folder, join=False):
+    #     tokens = p.split('_')
+    #     patient_id = tokens[0] + "_" + tokens[1]
+    #     contrast = tokens[-1].split('.')[0]
+    #     shutil.copy(join(input_folder, p),
+    #                 join(tmp_input_folder, patient_id + "_" + contrast_to_number[contrast] + ".nii.gz"))
 
     # run nnunet inference
-    tmp_output_folder_inferencia = join(tmp_output_folder, 'raw_output_1')
+    # tmp_output_folder_inferencia = join(tmp_output_folder, 'raw_output_1')
     os.system(
         "nnUNet_predict -i {} -o {} -t 600 -m 3d_fullres -tr diplomovka_nnUNetTrainer --save_npz".format(
-            tmp_input_folder, tmp_output_folder_inferencia))
-    convert_labels_back_to_BraTS_2018_2019_convention(join(tmp_output_folder, 'pp_output'),
-                                                      join(tmp_output_folder, 'pp_output_converted'))
-
-    for p in subfiles(join(tmp_output_folder, 'pp_output_converted'), join=False):
-        patient_id = p.split('_')[1].split('.')[0]
-        shutil.copy(join(tmp_output_folder, 'pp_output_converted', p), join(output_folder, patient_id + ".nii.gz"))
+            input_folder, output_folder))
+    # convert_labels_back_to_BraTS_2018_2019_convention(join(output_folder, 'pp_output'),
+    #                                                   join(output_folder, 'pp_output_converted'))
+    #
+    # for p in subfiles(join(output_folder, 'pp_output_converted'), join=False):
+    #     patient_id = p.split('_')[1].split('.')[0]
+    #     shutil.copy(join(output_folder, 'pp_output_converted', p), join(output_folder, patient_id + ".nii.gz"))
 
 
 if __name__ == '__main__':
