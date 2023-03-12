9e983de2df3f68abba63858cb9896bf8bda06d63
dominikaxx <domca.moly@gmail.com>
2023-03-12 18:02:58 +0100

    more experiments

--- nnunet/network_architecture/RSE_unet.py
+++ nnunet/network_architecture/RSE_unet.py
@@ -25,6 +25,41 @@ from nnunet.network_architecture.neural_network import SegmentationNetwork
 from nnunet.utilities.nd_softmax import softmax_helper
 
 
+class RSE_Block(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=16):
+        """
+        :param num_channels: No of input channels
+        :param reduction_ratio: By how much should the num_channels should be reduced
+        """
+        super(RSE_Block, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        num_channels_reduced = num_channels // reduction_ratio
+        print("RSE_Block num_channels : ", num_channels)
+        print("RSE_Block reduction_ratio : ", reduction_ratio)
+        print("RSE_Block num_channels_reduced : ", num_channels_reduced)
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.Linear(num_channels, num_channels_reduced, bias=True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(num_channels_reduced, num_channels, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: X, shape = (batch_size, num_channels, D, H, W)
+        :return: output tensor
+        """
+        batch_size, num_channels, D, H, W = input_tensor.size()
+        # Average along each channel
+        squeeze_tensor = self.avg_pool(input_tensor)
+
+        # channel excitation
+        fc_out_1 = self.relu(self.fc1(squeeze_tensor.view(batch_size, num_channels)))
+        fc_out_2 = self.sigmoid(self.fc2(fc_out_1))
+
+        output_tensor = torch.mul(input_tensor, fc_out_2.view(batch_size, num_channels, 1, 1, 1))
+        return output_tensor
+
+
 class ConvDropoutNormNonlin(nn.Module):
     """
     fixes a bug in ConvDropoutNormNonlin where lrelu was used regardless of nonlin. Bad.
